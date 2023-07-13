24b32f08a9a022015aa610292afa8505a9cf497e
nayan97 <nayanislam085@gmail.com>
2023-07-13 23:30:01 +0600

    basic dom

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const btn = document.querySelector('.btn');
+const h1= document.querySelector('h1');
+
+// btn.addEventListener('click', ()=>{
+//  alert('are you sure')
+// });
+
+// btn.addEventListener('click', btChange);
+
+// function btChange(){
+//  alert()
+// };
+
+// h1.onclick = () => {
+//  alert()
+// }
