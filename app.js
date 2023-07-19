9742eba1ae1dee48fa4086a9e5ef1ebfd949531e
nayan97 <nayanislam085@gmail.com>
2023-07-19 12:14:26 +0600

    basic alaram

--- app.js
+++ app.js
@@ -3,6 +3,32 @@
 const add = document.getElementById('add');
 const del = document.getElementById('del');
 const output = document.getElementById('data');
+const alts = document.getElementById('alts');
+const alte = document.getElementById('alte');
+
+
+let conut =0;
+let stopCount = '';
+
+add.onclick = () =>{
+    
+    stopCount =setInterval(() => {
+    conut++;
+    output.innerHTML= conut;
+    if( conut >= 10){
+       clearInterval(stopCount);
+        
+        alte.play();
+        conut = 0;
+    } else {
+       
+    }
+
+
+ }, 1000)
+}
+
+
 
 // btn.addEventListener('click', ()=>{
 //  alert('are you sure')
@@ -18,24 +44,32 @@ const output = document.getElementById('data');
 //  alert()
 // }
 
-let count = 0;
-add.onclick = () =>{
-    count++
-    if (count <= 10) {
-        output.innerHTML  = count;
-    } else {
-        count = 10;
-    }
+
+
+//  for manual counter
+
+// let count = 0;
+// add.onclick = () =>{
   
-}
+//     count++
+//     if (count <= 10) {
+//         output.innerHTML  = count;
+//     } else {
+//         count = 10;
+//         alts.play()
+//     }
+  
+// }
 
-del.onclick = () => {
-    count--
-
-    if(count >= 0){
-        output.innerHTML = count;
-    } else {
-        count = 0;
-    }
+// del.onclick = () => {
     
-}
+//     count--
+
+//     if(count >= 0){
+//         output.innerHTML = count;
+//     } else {
+//         count = 0;
+//         alte.play()
+//     }
+    
+// }
